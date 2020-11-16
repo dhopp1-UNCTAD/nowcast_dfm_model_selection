f6988760fc26caf4afd2ea0277d86110a9df4f75
dhopp1 <daniel.hopp1@gmail.com>
2020-11-16 12:51:06 +0100

    Same runs on DFM and LSTM

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -47,3 +47,56 @@ for target_variable in ranking.target_variable.unique():
 				)
 		
 		eval_df.to_csv("evaluation.csv", index=False)
+
+
+# vs DFM
+vs = pd.read_csv("dfm_vs_lstm.csv")
+for row in range(len(vs)):
+	print(row)
+	target_variable = vs.iloc[row, 0]
+	# getting which variables from the DFM run
+	variables = pd.DataFrame(vs.iloc[row, 25:]).reset_index()
+	variables.columns = ["variable", "in"]
+	variables = list(variables.loc[variables["in"] != 0,:].sort_values(["in"]).variable)
+	variables = [x for x in variables if x != target_variable]
+	
+	# params
+	n_timesteps = 12
+	train_episodes = 200
+	batch_size = 30
+	lr = 1e-2
+	decay = 0.98
+	missings = 0.0
+	n_hidden = 20
+	n_layers = 2
+	dropout = 0
+	
+	convergence_plot, eval_plot, eval_df = run_model.run_model(
+				variables = variables, 
+				target_variable = target_variable, 
+				n_timesteps = n_timesteps, 
+				train_episodes = train_episodes, 
+				batch_size = batch_size, 
+				lr = lr, 
+				decay = decay, 
+				missings = missings, 
+				n_hidden = n_hidden, 
+				n_layers = n_layers, 
+				dropout = dropout
+				)
+	
+	# filling in df
+	vs.loc[row, "RMSE_2_back_LSTM"] = eval_df.iloc[-1,:]["RMSE_2_back"]
+	vs.loc[row, "RMSE_1_back_LSTM"] = eval_df.iloc[-1,:]["RMSE_1_back"]
+	vs.loc[row, "RMSE_0_back_LSTM"] = eval_df.iloc[-1,:]["RMSE_0_back"]
+	vs.loc[row, "RMSE_1_ahead_LSTM"] = eval_df.iloc[-1,:]["RMSE_1_ahead"]
+	vs.loc[row, "RMSE_2_ahead_LSTM"] = eval_df.iloc[-1,:]["RMSE_2_ahead"]
+	vs.loc[row, "RMSE_LSTM"] = eval_df.iloc[-1,:]["RMSE"]
+	vs.loc[row, "MAE_2_back_LSTM"] = eval_df.iloc[-1,:]["MAE_2_back"]
+	vs.loc[row, "MAE_1_back_LSTM"] = eval_df.iloc[-1,:]["MAE_1_back"]
+	vs.loc[row, "MAE_0_back_LSTM"] = eval_df.iloc[-1,:]["MAE_0_back"]
+	vs.loc[row, "MAE_1_ahead_LSTM"] = eval_df.iloc[-1,:]["MAE_1_ahead"]
+	vs.loc[row, "MAE_2_ahead_LSTM"] = eval_df.iloc[-1,:]["MAE_2_ahead"]
+	vs.loc[row, "MAE_LSTM"] = eval_df.iloc[-1,:]["MAE"]
+	
+	vs.to_csv("dfm_vs_lstm.csv", index=False)
